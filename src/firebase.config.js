3398be00d5117acb7b7a5ef4229ac975629ff9ef
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-10-19 16:02:57 +0600

    firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyATclDM_DfNuTiExV1Tuv-f1ds5ctiiSpw",
+    authDomain: "creative-agency-7214d.firebaseapp.com",
+    databaseURL: "https://creative-agency-7214d.firebaseio.com",
+    projectId: "creative-agency-7214d",
+    storageBucket: "creative-agency-7214d.appspot.com",
+    messagingSenderId: "89310232324",
+    appId: "1:89310232324:web:04e7d76ffc40a909a8da51"
+  };
